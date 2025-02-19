e8054722324ca6aab20b8512eaa409a0fd537ca8
Enoch Oyeniyi <enoch.oyeniyi@stu.ocsb.ca>
2025-02-19 17:22:42 +0000

    Added area_per_rect.py

---
+++ area_per_rect.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# Created By: Enoch O
+# Date: Feb 19 2025
+# This program calculates the area & perimeter of a 7cm X 2cm rectangle.
+
+
+def main():
+    print("For a rectangle with length 7cm and width 2cm:")
+
+    print("The area is: {}cm^2".format(7 * 2))
+    print()
+
+    print("The perimeter is: {}cm".format(2 * (7 + 2)))
+
+
+if __name__ == "__main__":
+    main()
